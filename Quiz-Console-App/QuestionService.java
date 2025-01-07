54a2ab29b8fc03f7b9b74cb8450ac3a213273296
aakashdeep2310 <rjaakashdeep007@gmail.com>
2025-01-07 10:42:15 +0530

    updated the code to print the answers

--- Quiz-Console-App/QuestionService.java
+++ Quiz-Console-App/QuestionService.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class QuestionService {
 
     Question[] questions = new Question[5];
+    String selection[] = new String[5];
 
     public QuestionService(){
         questions[0] = new Question(1, "What is this size of int", "2", "4" , "6","8", "4" ); 
@@ -12,11 +15,25 @@ public class QuestionService {
      }
 
 
-    public void displayQuestions(){
-        // System.out.println("Display question");
-        // questions[0] = new Question();
+    public void playQuiz(){
+       
+        int i = 0;
         for(Question q : questions){
-            System.out.println(q);
+            System.out.println("Question no. : "+q.getId());
+            System.out.println(q.getQuestion());
+            System.out.println(q.getOpt1());
+            System.out.println(q.getOpt2());
+            System.out.println(q.getOpt3());
+            System.out.println(q.getOpt4());
+
+            Scanner sc = new Scanner(System.in);
+            selection[i++] = sc.nextLine();
+
+            // System.out.println(q.getAnswer());
+        }
+
+        for(String s : selection){
+            System.out.print(s + " ");
         }
     }
 

d6dd16e74f214037f04569059fd10cb526805fa2
aakashdeep2310 <rjaakashdeep007@gmail.com>
2025-01-10 22:56:30 +0530

    Collection APT

---
+++ Collection/Demo.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Demo {
+
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        int a = 10;
+        Collection <Integer> nums = new ArrayList<Integer>();
+        // Collection nums = new ArrayList();
+        nums.add(a);
+        nums.add(3);
+        nums.add(4);
+        nums.add(4);
+        nums.add(8);
+
+        // System.out.println(nums);
+        
+        for(int n : nums){
+            System.out.print(n+ " ");
+        }
+        // for(Object n : nums){
+        //     int num = (int)n;
+        //     System.out.print(num *2+ " ");
+        // }
+    }
+}
